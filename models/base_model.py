f0f846d16ad3eb30bc2a9245c83276e53ecfc078
yaniG <yane12girma@gmail.com>
2024-01-09 05:00:06 -0500

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,25 @@
+#!/usr/bin/python3
+""" Module: base.py """
 
+from datetime import datetime
+import uuid
+import models
+
+class BaseModel:
+   """Base class for all our classes"""
+  
+  def __init__(self, *args, **kwargs):
+        """ deserialize and serialize a class """
+        if kwargs:
+            for k, v in kwargs.items():
+                if k != '__class__':
+                    setattr(self, k, v)
+            s = '%Y-%m-%dT%H:%M:%S.%f'
+            self.created_at = datetime.strptime(self.created_at, s)
+            self.updated_at = datetime.strptime(self.updated_at, s)
+        else:
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            self.id = str(uuid.uuid4())
+            models.storage.new(self)
+  
