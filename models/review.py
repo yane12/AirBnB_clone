5a2d1daf57bf69a494cbe840ed8b7f2ce0935bda
yaniG <yane12girma@gmail.com>
2024-01-16 05:51:09 -0500

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-"""A class Review that inherits from BaseModel"""
+"""Defines the Review class."""
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """Review class"""
+    """Represent a review.
+
+    Attributes:
+        place_id (str): The Place id.
+        user_id (str): The User id.
+        text (str): The text of the review.
+    """
+
     place_id = ""
     user_id = ""
     text = ""
