39641c201318c27342627c463c48a66949681fb2
yane12 <yane12girma@gmail.com>
2024-01-16 02:36:44 -0800

    test_city.py

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""amenity class unitests"""
+import unittest
+from datetime import datetime
+import time
+from models.amenity import Amenity
+import re
+import json
+from models.engine.file_storage import FileStorage
+import os
+from models import storage
+from models.base_model import BaseModel
+
+
+class TestAmenity(unittest.TestCase):
+
+    """Test Cases for the Amenity class."""
+
+    def setUp(self):
+        """Sets up test methods."""
+        pass
+
+    def tearDown(self):
+        """Tears down test methods."""
+        self.resetStorage()
+        pass
+
+    @classmethod
+    def resetStorage(self):
+        """Resets FileStorage data."""
+        FileStorage._FileStorage__objects = {}
+        if os.path.isfile(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
+
+    def test_8_instantiation(self):
+        """Tests instantiation of Amenity class."""
+
+        b = Amenity()
+        self.assertEqual(str(type(b)), "<class 'models.amenity.Amenity'>")
+        self.assertIsInstance(b, Amenity)
+        self.assertTrue(issubclass(type(b), BaseModel))
+        self.assertTrue(hasattr(b, "id"))
+        self.assertTrue(hasattr(b, "created_at"))
+        self.assertTrue(hasattr(b, "updated_at"))
+        self.assertIn('name', dir(Amenity))
+        self.assertEqual(str, type(Amenity.name))
+
+if __name__ == "__main__":
+    unittest.main()
